f4ca8d58e56b456b4f9d1ad35e084e6d89e19491
herooooooooo <kevolig1234@gmail.com>
2019-04-12 17:35:43 +0400

    just delete the . in the end and fix D.35

--- ML/clean.R
+++ ML/clean.R
@@ -5,28 +5,34 @@ df <- read_excel("2019-2.xls", sheet = "2019_Փետրվար")
 
 df <- df[,c(1,2)]
 df <- df[-c(1:45),]
-
+df <- df[-313,]
 empty_rows(df)
 
 df <- remove_empty_rows(df)
-df$id="0"
+df$ID="0"
 
 colnames(df) <- c("rownumber","name","ID")
 
 group_pos <- grep('B|C|D',df$rownumber)
-sub_group_pos <- grep('[0-9]{2}.', df$rownumber)
-sub_sub_group_pos <- grep('[0-9]{2}.[0-9]+',df$rownumber)
+sub_group_pos <- grep('[0-9]{2}\\.', df$rownumber)
+sub_sub_group_pos <- grep('[0-9]{2}\\.[0-9]+',df$rownumber)
 sub_group_pos <- sub_group_pos[!sub_group_pos %in% sub_sub_group_pos]
 
 temp <- df
-
+j = 1
 for (i in 1:nrow(temp)) {
+  
   if (i %in% group_pos) {
-    print(paste("in group_pos", i))
+    temp1 <-str_extract(df$rownumber[i],'B|C|D')
+    temp$ID[i] = temp1
+    j = 1
   } else if(i %in% sub_group_pos) {
-    print(paste("in sub_group_pos", i))
+    temp2 <- str_extract(df$rownumber[i],'[0-9]{2}\\.')
+    j = 1
+    temp$ID[i] <- paste0(temp1,temp2)
   }
   else {
-    print("just a normal row")
-  }
+    temp$ID[i] <- paste0(temp1,".",temp2,j)
+    j = j + 1
+    }
 }
