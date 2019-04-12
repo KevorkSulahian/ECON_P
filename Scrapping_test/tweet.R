92566a24d20a49d0650996a89af3e88e3bdd9918
herooooooooo <kevolig1234@gmail.com>
2019-04-12 09:19:23 +0400

    added ML clean

--- Scrapping_test/tweet.R
+++ Scrapping_test/tweet.R
@@ -2,10 +2,10 @@ library(ROAuth)
 library(rtweet)
 library(twitteR)
 
-api_key <- "STBRYKp94wAGStQ0j0BA4h8eq"
-api_secret <- "aRuTMkDaBONAQAR1ElNl18Ol0wzAHNvqUa0CE0cYWr3GtpwKdS"
-access_token <- "1847123360-E2tPObEpAGXAonLgXDlYTgemO93Sjos7Zk8q14l"
-access_token_secret <- "20u0suiEauEHc8KQNFp82ckj7vWYdn3FTiK6kk5iNLPqu"
+api_key <- ""
+api_secret <- ""
+access_token <- ""
+access_token_secret <- ""
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
 # had to run this to make it work
