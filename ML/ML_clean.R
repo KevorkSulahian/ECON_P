aa39c8dddac56c5671fa0ec45190031866ff5a7a
herooooooooo <kevolig1234@gmail.com>
2019-05-16 22:05:45 +0400

    final commit from your second laptop :'( I love you <3

--- ML/ML_clean.R
+++ ML/ML_clean.R
@@ -2,4 +2,23 @@ library(readxl)
 library(sjmisc)
 library(stringr)
 
-df <- read_excel("2012.xlsx", sheet = "Հունվար")
+df <- read_excel("2017.xlsx", sheet = "2017 Հունվար")
+
+df <- df[-c(1:45),]
+
+colnames(df) <- c("one","two","three", "four", "five","six", "seven", "eight", "nine", "ten", "eleven",
+                  "tweleve", "thirteen", "fourtheen", "fifteen", "sixteen", "seventeen", "eighteen", 'nineteen', 'twenty')
+
+
+df$one <- as.numeric(df$one)
+
+df <- df[complete.cases(df[,1]),]
+df$one <- NULL
+
+df[,-1] <- sapply(df[,-1], as.numeric)
+df <- df[complete.cases(df[,1]),]
+
+writexl::write_xlsx(df, "ML_files/2016_DEC.xlsx")
+
+
+
