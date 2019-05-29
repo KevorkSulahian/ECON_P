f16ca471e1c803ec407785e96b33383cc19da54f
KevorkSulahian <kevolig1234@gmail.com>
2019-05-29 15:45:21 +0400

    pushing

--- ML/ML_clean.R
+++ ML/ML_clean.R
@@ -2,7 +2,7 @@ library(readxl)
 library(sjmisc)
 library(stringr)
 
-df <- read_excel("2017.xlsx", sheet = "2017 Հունվար")
+df <- read_excel("2017.xlsx", sheet = "2017 Դեկտեմբեր")
 
 df <- df[-c(1:45),]
 
@@ -18,7 +18,8 @@ df$one <- NULL
 df[,-1] <- sapply(df[,-1], as.numeric)
 df <- df[complete.cases(df[,1]),]
 
-writexl::write_xlsx(df, "ML_files/2016_DEC.xlsx")
+writexl::write_xlsx(df, "ML_files/2017_DEC.xlsx")
 
+rnorm(1,1,3)
 
 
